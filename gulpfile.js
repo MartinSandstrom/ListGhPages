d11da77713361cc2987630c0ac017c8f611c4f41
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-11-18 12:54:11 +0100

    Rewriting solution to detect ghpages.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 
 gulp.task('compress', function() {
-  return gulp.src('scripts/*.js')
+  return gulp.src('./scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src('styles/*.css')
+  return gulp.src('./styles/*.css')
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,52 @@
-angular.module('ghPagesApp', []).controller('mainController', ['$http', function($http){
+angular.module('ghPagesApp', []).controller('mainController', ['$http', '$q', function($http, $q){
 
-        var vm = this;
-        vm.title = 'List your github pages';
-        vm.username = 'MartinSandstrom';
-        vm.allGhPages = [];
+    var vm = this;
+    vm.title = 'List your github pages';
+    vm.username = 'MartinSandstrom';
+    vm.allGhPages = [];
 
-        var onSearchComplete = function(result){
-            var url = 'http://' + vm.username + '.github.io/';
-            result.data.forEach(function(data){
-                    if(data.fork) return;
+    var onSearchComplete = function(result){
+        var url = 'http://' + vm.username + '.github.io/';
+        result.data.forEach(function(data){
 
 
-                    var newUrl = url + data.name
-                    $http.get(newUrl).then(function(result){
-                        vm.allGhPages.push(newUrl);
-                    });
+            if(data.fork) return;
+
+            var hasGH;
+
+            hasGhPage(data.name).then(function(response){
+
+                 if(!response) return;
+
+                 var newUrl = url + data.name
+                 vm.allGhPages.push(newUrl);
+             });
+        });
+    }
+
+    var onError = function(err){
+        console.log(err);
+    }
+
+    vm.list = function(username){
+        $http.get('https://api.github.com/users/'+ username + '/repos')
+        .then(onSearchComplete, onError);
+    }
+
+    function hasGhPage(repo){
+        var deferred= $q.defer();
+        var url = 'https://api.github.com/repos/' + vm.username +'/' + repo + '/branches';
+        var hasGhPage = false;
+
+        $http.get(url).then(function(response){
+            response.data.forEach(function(data){
+                if(data.name === 'gh-pages'){
+                    hasGhPage = true;
+                }
             });
-        }
+            deferred.resolve(hasGhPage);
+        });
 
-        var onError = function(err){
-            console.log(err);
-        }
-
-        vm.list = function(username){
-            $http.get('https://api.github.com/users/'+ username + '/repos')
-            .then(onSearchComplete, onError);
-        }
+        return deferred.promise;
+    }
 }]);
